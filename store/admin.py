3839319ab77412f35c7b0f5cd38b33b9c64c5099
magister <you@example.com>
2025-08-15 09:36:35 +0500

    fix duplicate model registration and add auto superuser

--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
-from .models import Brand, Type, Product, Order,OrderItem, BirthdayClient
-
-admin.site.register(Brand)
-admin.site.register(Type)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(BirthdayClient)
-
-
-
-from django.contrib import admin
 from .models import Brand, Type, Product, Order, OrderItem, BirthdayClient
 from django.contrib.auth import get_user_model
 
+# Регистрируем модели один раз
 admin.site.register(Brand)
 admin.site.register(Type)
 admin.site.register(Product)
@@ -23,7 +12,7 @@ admin.site.register(BirthdayClient)
 
 # --- ВРЕМЕННО: авто-создание суперпользователя ---
 User = get_user_model()
-if not User.objects.filter(username="admin").exists():
+if not User.objects.filter(username="tuf").exists():
     User.objects.create_superuser(
         username="tuf",
         email="beka2007azimut@gmail.com",
