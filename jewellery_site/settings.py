301a5c861fe45dda06e151382cdbceec2ab92238
magister <you@example.com>
2025-10-04 17:58:05 +0800

    Auth modify

--- jewellery_site/settings.py
+++ jewellery_site/settings.py
@@ -57,11 +57,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # --- Middleware ---
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",   # ✅ обязательная строка
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
